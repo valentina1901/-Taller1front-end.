29e7712c60b04720ad07dec92c68f43b60b9c53f
valentina1901 <vgaviria415@misena.edu.co>
2022-09-18 23:41:37 -0500

    realizando tipo de triangulo

---
+++ js/tipotriangulo.js
@@ -0,0 +1,31 @@
+
+    
+
+    function resolver() {
+
+    let opcion= document.getElementById("accion").value 
+    if(opcion !='')
+    {
+        switch (opcion) {
+
+            //Equilatero 
+            case 'Tres lados iguales':
+
+            break;
+
+            //Isosceles
+            case 'Dos lados iguales':
+
+            break;
+
+            //Escaleno
+            case 'Tres lados desiguales':
+
+            
+            break;
+
+        }
+  
+        } else {alert("Error");}
+        
+    }
